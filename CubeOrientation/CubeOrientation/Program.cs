4e88237d7ab043cc4d9ddf21063c090504d139f7
DuaneCressman <49034609+DuaneCressman@users.noreply.github.com>
2021-09-02 17:33:14 -0400

    Added the blank project.

---
+++ CubeOrientation/CubeOrientation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CubeOrientation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
